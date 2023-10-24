74bb604a9b02c2d0c7bdd75b322fdb3dc17e7ff8
Muhammad Fraz <muhammad@testPro.io>
2023-10-23 23:03:27 -0400

    Test NG Overview
    Test NG Overview - In Class Exercise.

---
+++ src/test/java/ProfileTests.java
@@ -0,0 +1,60 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.UUID;
+
+public class ProfileTests extends BaseTest {
+
+    @Test
+    public void changeProfileName() throws InterruptedException {
+        navigateToLoginPage();
+        //Login
+        provideEmail("demo@class.com");
+        providePassword("te$t$tudent");
+        clickSubmit();
+        //Wait for 2 Seconds
+        Thread.sleep(2000);
+        //Click on Avatar to go to Profile Page
+        clickAvatar();
+        provideCurrentPassword("te$t$tudent");
+        //Enter Random Name
+        String randomName = generateRandomName();
+        provideProfileName(randomName);
+        clickSave();
+        //Wait
+        Thread.sleep(2000);
+        //Comparison
+        WebElement actualProfileName = driver.findElement(By.cssSelector("a.view-profile>span"));
+        Assert.assertEquals(actualProfileName.getText(), randomName);
+    }
+
+    public void clickAvatar(){
+        WebElement avatarIcon = driver.findElement(By.cssSelector("img.avatar"));
+        avatarIcon.click();
+    }
+
+    public void provideCurrentPassword(String password){
+        WebElement currentPassword = driver.findElement(By.cssSelector("[name='current_password']"));
+        currentPassword.clear();
+        currentPassword.sendKeys(password);
+    }
+
+    public String generateRandomName(){
+        return UUID.randomUUID().toString().replace("-","");
+    }
+
+    public void provideProfileName(String name){
+        WebElement profileName = driver.findElement(By.cssSelector("[name='name']"));
+        profileName.clear();
+        profileName.sendKeys(name);
+    }
+
+    public void clickSave(){
+        WebElement saveButton = driver.findElement(By.cssSelector("button.btn-submit"));
+        saveButton.click();
+    }
+
+
+}
