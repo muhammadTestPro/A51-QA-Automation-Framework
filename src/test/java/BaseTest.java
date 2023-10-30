aba82fe5bc43da9973308df9ed668500f4e0df48
Muhammad Fraz <muhammad@testPro.io>
2023-10-30 10:50:37 -0400

    A51-L19-Class_demo
    Without Excel Locations

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,21 +1,44 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.Reporter;
+import org.testng.annotations.*;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
+
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+
 
 public class BaseTest {
 
+    @DataProvider(name="LoginData")
+    public Object[][] getDataFromDataProvider(){
+        return new Object[][]{
+                {"demo@class.com", "te$t$tudent"},
+                {"invalidemail@class.com", "te$t$tudent"},
+                {"demo@class.com", "InvalidPassword"},
+                {"",""}
+        };
+    }
+
+    @DataProvider(name="excel-data")
+    public Object[][] excelDP() throws IOException {
+        Object[][] arrObj = getExcelData("test.xlsx", "test.xlsx");
+        return arrObj;
+    }
+
+
     public WebDriver driver;
-
-
-
     public String url = "https://qa.koel.app";
 
 
@@ -25,12 +48,14 @@ public class BaseTest {
     }
 
     @BeforeMethod
-    public void launchBrowser(){
+    @Parameters({"BaseURL"})
+    public void launchBrowser(String BaseURL){
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
+        navigateToLoginPage(BaseURL);
     }
 
     @AfterMethod
@@ -41,6 +66,10 @@ public class BaseTest {
 
     public void navigateToLoginPage(){
         driver.get(url);
+    }
+
+    public void navigateToLoginPage(String BaseURL){
+        driver.get(BaseURL);
     }
 
     public void provideEmail(String email){
@@ -59,4 +88,37 @@ public class BaseTest {
         WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
         submit.click();
     }
+
+
+    public String [][] getExcelData(String fileName, String sheetName){
+        String [][] data = null;
+        try{
+            FileInputStream fileInputStream = new FileInputStream(fileName);
+            XSSFWorkbook wb = new XSSFWorkbook(fileInputStream);
+            XSSFSheet sheet = wb.getSheet(sheetName);
+            XSSFRow row = sheet.getRow(0);
+
+            int numOfRows = sheet.getPhysicalNumberOfRows();
+            int numOfColumns = row.getLastCellNum();
+
+            XSSFCell cell;
+
+            data = new String[numOfRows-1][numOfColumns];
+
+            for(int i = 1; i < numOfRows; i++){
+                for (int j = 0; j < numOfColumns; j++){
+                    row = sheet.getRow(i);
+                    cell = row.getCell(j);
+                    data [i-1][j] = cell.getStringCellValue();
+                }
+            }
+        } catch(Exception e){
+            System.out.println("Something went wrong." +e);
+        }
+        return data;
+    }
+
+
+
+
 }
